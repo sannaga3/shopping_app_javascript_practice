48df60c7471e69640be4cdc9dcb334ad7b2d8297
sannaga3 <n.tetsuya02@gmail.com>
2021-08-13 10:48:30 +0900

    ItemとShopのcreateアクションをajax化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,6 @@ class ItemsController < ApplicationController
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
-
     respond_to do |format|
       if @item.save
         format.html { redirect_to @item, notice: "Item was successfully created." }
@@ -51,7 +50,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
+      format.html { redirect_to shops_path, notice: "Item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :shop
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :stock
+    validates :comment
+  end
 end
