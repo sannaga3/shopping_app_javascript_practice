cee256c26dd615c55c61353890a9fc4a6ff1524c
sannaga3 <n.tetsuya02@gmail.com>
2021-08-13 18:08:27 +0900

    deviseにてuserモデル及びコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    get '/users/sign_out', to: 'users/sessions#destroy'
+    root "users/sessions#new"
+  end
   resources :items
   resources :shops
-  root to: "shops#index"
+  # root to: "shops#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
